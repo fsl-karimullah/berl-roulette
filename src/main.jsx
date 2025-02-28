5cea93eda3f45d3f067654d6df210b39fc078080
fsl-karimullah <faisalbic123@gmail.com>
2025-02-28 16:26:05 +0700

    sisa api

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,9 @@ import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/roulette-new/">
+    <BrowserRouter basename="/demo-roulette/">
       <App />
     </BrowserRouter>
   </StrictMode>
 );
+ 
